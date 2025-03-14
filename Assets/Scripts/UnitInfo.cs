8ef0fde144b0801e930dbe296bf36bb4a837ead3
Bife <tomsy.ss@gmail.com>
2025-03-14 19:11:57 +0000

    Initializing

---
+++ Assets/Scripts/UnitInfo.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class UnitInfo : ScriptableObject
+{
+	private string unitName;
+	
+	private int health;
+	private int defense;
+	private int dodge;
+	private int accuracy;
+	private int speed;
+	private int minDamage;
+	private int maxDamage;
+	private int critChance;
+}
